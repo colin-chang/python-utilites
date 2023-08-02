d47d4469a597d20439e90e40bd657ed81902fb0e
Colin <zhangcheng5468@gmail.com>
2023-08-02 15:20:54 +0800

    重构SqlHelper

--- utility/SqlHelper.py
+++ utility/SqlHelper.py
@@ -3,53 +3,76 @@ import logging
 
 
 class MySqlHelper(object):
-    def __init__(self, **kwargs):
-        self.__connection = connect(**kwargs)
-        self.__cursor = self.__connection.cursor()
+    """SqlHelper for MySQL"""
 
-    def __enter__(self):
-        return self
+    def __init__(self, host, user, password, database, port=3306, charset="utf8"):
+        self.__host = host
+        self.__port = port
+        self.__user = user
+        self.__password = password
+        self.__database = database
+        self.__charset = charset
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.__connection is not None:
-            self.__connection.close()
-        if self.__cursor is not None:
-            self.__cursor.close()
+    def __execute(self, fun):
+        with connect(host=self.__host, port=self.__port, user=self.__user, password=self.__password,
+                     database=self.__database,
+                     charset=self.__charset).cursor() as cur:
+            return fun(cur)
 
     def execute_datatable(self, sql, *args):
-        self.__cursor.execute(sql, args)
-        return self.__cursor.fetchall()
+        """fetch all data records by once"""
+
+        def execute(cur):
+            cur.execute(sql, *args)
+            return cur.fetchall()
+
+        return self.__execute(execute)
 
     def execute_datareader(self, sql, *args):
+        """generator of the query result"""
 
-        self.__cursor.execute(sql, args)
-        while True:
-            row = self.__cursor.fetchone()
-            if not row:
-                break
-            yield row
+        def execute(cur):
+            cur.execute(sql, *args)
+            while True:
+                row = cur.fetchone()
+                if not row:
+                    break
+                yield row
 
-    def execute_nonquery(self, sql, *args, many=False):
-        try:
-            if many:
-                affected = self.__cursor.executemany(sql, [("Tom", 1, 2), ("Jerry", 2, 1)])
-            else:
-                affected = self.__cursor.execute(sql, args)
-            self.__cursor.connection.commit()
-            return affected
-        except Exception as ex:
-            self.__cursor.connection.rollback()
-            logging.error(ex, exc_info=1, stack_info=1)
-            return 0
+        return self.__execute(execute)
+
+    def execute_nonquery(self, sql, *args):
+        """
+        execute nonquery including insert/update/delete.it supports batch records by transaction
+        :param sql:
+        :param args:
+        :return:
+        """
+
+        def execute(cur):
+            try:
+                if len(*args) > 1:
+                    affected = cur.executemany(sql, *args)
+                else:
+                    affected = cur.execute(sql, *args)
+
+                cur.connection.commit()
+                return affected
+            except Exception as ex:
+                cur.connection.rollback()
+                logging.error(ex, exc_info=1, stack_info=1)
+                return 0
+
+        return self.__execute(execute)
 
 
 if __name__ == '__main__':
     db_config = {
-        'host': '192.168.192.5',
+        'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': 'xiaoyang@123123@db',
-        'database': 'hc-dev-gam'
+        'password': '123456',
+        'database': 'test'
     }
     with MySqlHelper(**db_config) as helper:
         students = helper.execute_datatable("select * from students where id<=%s", 4)
