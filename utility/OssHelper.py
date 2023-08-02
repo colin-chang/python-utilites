fd5231eded9db1e36e416828b6d18168b952d0d8
Colin <zhangcheng5468@gmail.com>
2023-08-02 11:32:01 +0800

    重构OssHelper

--- utility/OssHelper.py
+++ utility/OssHelper.py
@@ -1,5 +1,6 @@
-from oss2 import Auth, Bucket
-from os.path import exists, dirname
+from oss2 import Auth, Bucket, ObjectIterator
+from itertools import islice
+from os.path import exists, dirname, isfile
 from os import mkdir
 
 
@@ -8,18 +9,37 @@ class OssHelper(object):
         auth = Auth(**kwargs.get('auth'))
         self.__bucket = Bucket(auth, **kwargs.get('bucket'))
 
-    def download(self, key, dest):
-        if exists(dest):
-            return
+    def upload(self, key, filename, **kwargs):
+        """
+        上传文件
+        """
+        if not exists(filename):
+            raise FileNotFoundError()
+        if not isfile(filename):
+            raise Exception(f'{filename} must be a file')
+        self.__bucket.put_object_from_file(key, filename, **kwargs)
 
-        downloads = dirname(dest)
+    def download(self, key, filename, **kwargs):
+        """
+        下载文件
+        """
+        if exists(filename):
+            raise FileExistsError()
+
+        downloads = dirname(filename)
         if len(downloads) > 0 and not exists(downloads):
             mkdir(downloads)
 
-        self.__bucket.get_object_to_file(key, dest)
+        self.__bucket.get_object_to_file(key, filename, **kwargs)
 
-    def delete(self, key):
-        return self.__bucket.delete_object(key)
+    def list_objects(self, prefix='', delimiter='', marker='', max_keys=100, **kwargs):
+        return islice(ObjectIterator(self.__bucket, prefix, delimiter, marker, max_keys, **kwargs), max_keys)
+
+    def delete(self, key, **kwargs):
+        """
+        删除文件
+        """
+        return self.__bucket.delete_object(key, **kwargs)
 
 
 if __name__ == '__main__':
@@ -33,8 +53,16 @@ if __name__ == '__main__':
             'bucket_name': ''
         }
     }
-    oss = OssHelper(**oss_config)
-    oss.download('prod/photos/002163ecc833e4fae5783e3a2301c18c.jpg', '/Users/Colin/Downloads/sample.jpg')
-    print('downloaded')
 
-    # oss.delete('dev/photos/002163ecc833e4fae5783e3a2301c18c.jpg')
+    oss = OssHelper(**oss_config)
+    ps = {
+        'key': 'dev/photos/002163ecc833e4fae5783e3a2301c18c.jpg',
+        'filename': '/Users/Colin/Downloads/sample.jpg'
+    }
+    # oss.upload(**ps)
+    # oss.download(**ps)
+    objs = oss.list_objects(prefix='dev/photos', max_keys=5)
+    for obj in objs:
+        print(obj.key)
+
+    # oss.delete(ps.get('key'))
