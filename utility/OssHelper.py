6afd0e0002786557a5ac8a1071a1eb9c6f3b218e
Colin <zhangcheng5468@gmail.com>
2023-08-03 19:11:54 +0800

    更新OssHelper批量删除方法

--- utility/OssHelper.py
+++ utility/OssHelper.py
@@ -35,18 +35,36 @@ class OssHelper(object):
     def list_objects(self, prefix='', delimiter='', marker='', max_keys=100, **kwargs):
         return islice(ObjectIterator(self.__bucket, prefix, delimiter, marker, max_keys, **kwargs), max_keys)
 
-    def delete(self, key, **kwargs):
+    def delete(self, *keys, **kwargs):
         """
         删除文件
         """
-        return self.__bucket.delete_object(key, **kwargs)
+        max_keys = 1000
+
+        if len(keys) <= 0:
+            return []
+
+        if len(keys) <= max_keys:
+            return self.__bucket.batch_delete_objects(list(keys), **kwargs).deleted_keys
+
+        deleted = []
+        current_list = []
+        for key in keys:
+            current_list.append(key)
+            if len(current_list) >= max_keys:
+                deleted.extend(self.delete(*current_list))
+                current_list = []
+        if len(current_list) > 0:
+            deleted.extend(self.delete(*current_list))
+
+        return deleted
 
 
 if __name__ == '__main__':
     oss_config = {
         'auth': {
-            'access_key_id': '',
-            'access_key_secret': ''
+            "access_key_id": "",
+            "access_key_secret": ""
         },
         'bucket': {
             'endpoint': 'https://oss-cn-beijing.aliyuncs.com',
@@ -55,14 +73,16 @@ if __name__ == '__main__':
     }
 
     oss = OssHelper(**oss_config)
-    ps = {
-        'key': 'dev/photos/002163ecc833e4fae5783e3a2301c18c.jpg',
-        'filename': '/Users/Colin/Downloads/sample.jpg'
-    }
+    # ps = {
+    #     'key': 'dev/photos/002163ecc833e4fae5783e3a2301c18c.jpg',
+    #     'filename': '/Users/Colin/Downloads/sample.jpg'
+    # }
     # oss.upload(**ps)
     # oss.download(**ps)
-    objs = oss.list_objects(prefix='dev/photos', max_keys=5)
-    for obj in objs:
-        print(obj.key)
+    # objs = oss.list_objects(prefix='dev/photos', max_keys=5)
+    # for obj in objs:
+    #     print(obj.key)
 
-    # oss.delete(ps.get('key'))
+    print(oss.delete(
+        'prod/videos/3a0cad5b-8f25-45e0-5ef8-f63d267ac754.mp4',
+        'prod/videos/3a0cc21c-ef42-39ee-ac7e-556f6924bfe9.mp4'))
